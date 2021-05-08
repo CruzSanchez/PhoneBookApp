63596e230aac2599de0a3682494f213e1b77473f
Seth Bowman <brandonsethbowman@gmail.com>
2021-05-07 19:25:04 -0500

    Add project files.

---
+++ PhonebookConsoleUI/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using PhonebookLibrary;
+
+namespace PhonebookConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Phonebook Application.");
+            Console.WriteLine("This application is used to create, update, delete, and read back contacts in a database.");
+
+            Console.WriteLine("Would you like to create a contact?");
+            var answer = Console.ReadLine();
+            if(answer == "yes")
+            {
+                Phonebook.CreateContact();
+                
+            }
+            else if (string.IsNullOrEmpty(answer))
+            {
+                Console.WriteLine("You failed to type anything.");
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine("You've chosen not to create a contact.");
+            }
+
+
+            Console.WriteLine("Would you like to look at your contact list?");
+
+            answer = Console.ReadLine();
+
+            if(answer == "yes")
+            {
+                Phonebook.ReadBackContacts();
+            }
+            else if (string.IsNullOrEmpty(answer))
+            {
+                Console.WriteLine("You failed to type anything.");
+            }
+            else if (answer == "no")
+            {
+                Console.WriteLine("You have chosen not to read back your contact list.");
+            }
+
+            Console.WriteLine();
+
+
+            
+        }
+    }
+}
---
+++ PhonebookLibrary/Phonebook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhonebookLibrary
+{
+    public static class Phonebook
+    {
+        public static List<Contact> Contacts { get; private set; } = new List<Contact>();
+
+        public static void CreateContact()
+        {
+            var contact = new Contact();
+            Console.WriteLine("Please enter the name of your contact:");
+            contact.Name = Console.ReadLine();
+            Console.WriteLine("Now enter the number of your contact:");
+            contact.Number = Console.ReadLine();
+            Console.WriteLine("Please enter the email for your contact:");
+            contact.Email = Console.ReadLine();
+            Console.WriteLine("Now enter the DOB of your contact:");
+            contact.DOB = DateTime.Parse(Console.ReadLine());
+
+            AddContactToList(contact);
+        }
+
+        private static void AddContactToList(Contact contact)
+        {
+            Contacts.Add(contact);
+        }
+
+        public static void ReadBackContacts()
+        {
+            foreach (var contact in Phonebook.Contacts)
+            {
+                Console.WriteLine($"Name :{contact.Name} | Phone #: {contact.Number} | E-Mail: {contact.Email} | DOB: {contact.DOB}");
+            }
+        }
+    }
+}
